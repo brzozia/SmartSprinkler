49c557abf6a8929c897cf8123a2dc3ba9d5e609c
lewelyn7 <karol.hamielec@gmail.com>
2021-04-12 22:58:52 +0200

    error red light

--- GeneralLogger/GeneralLogger.cpp
+++ GeneralLogger/GeneralLogger.cpp
@@ -2,5 +2,8 @@
 
 class GeneralLogger: Logging{
 
-
+    template <class T, typename... Args> void error(T msg, Args... args){
+        Logging::error(msg, args...);
+        //TODO blink red light
+    }
 }
