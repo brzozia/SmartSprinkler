b120ad4ab3d7bb2655fe4759a830ce4b542a0561
lewelyn7 <karol.hamielec@gmail.com>
2021-04-14 20:49:34 +0200

    logger structure clean up

--- GeneralLogger/GeneralLogger.cpp
+++
@@ -1,9 +0,0 @@
-#include <ArduinoLog.h>
-
-class GeneralLogger: Logging{
-
-    template <class T, typename... Args> void error(T msg, Args... args){
-        Logging::error(msg, args...);
-        //TODO blink red light
-    }
-}
--- src/GeneralLogger/GeneralLogger.cpp
+++ src/GeneralLogger/GeneralLogger.cpp
@@ -1,6 +1,9 @@
 #include <ArduinoLog.h>
 
 class GeneralLogger: public Logging{
-
+    template <class T, typename... Args> void error(T msg, Args... args){
+        Logging::error(msg, args...);
+        //TODO blink red light
+    }
 
 };
